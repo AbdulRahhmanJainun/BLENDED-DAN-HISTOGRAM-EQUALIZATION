57575433e267f872048628a9fbda07cb4f3c03e3
AbdulRahhmanJainun <abrajarahman@gmail.com>
2024-10-18 15:58:01 +0700

    Update equal.py

--- equal.py
+++ equal.py
@@ -3,8 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def equalization(image1,op1,image2,op2):
-    hist,bins = np.histogram(image)
-    img2 = image2.astype(np.float32)
-    imgBlend = (img1*op1)+(img2*op2)
-    imgBlend = np.clip(imgBlend,0,255)
-    return imgBlend.astype(np.uint8)
+    hist,bins = np.histogram(image.fllaten(),bins=256,range=[0,256])
+    cdf = hist.cumsum()
+    cdf_normalized =(cdf/cdf.max()) *255
+    imgEqual = np.interp(image.flatten(),bins[:-1],cdf_normalized)
+    return imgEqual.reshape(image.shape).astype(np.uint8)
+
+img1 = img.imread("C:\\gambar\\istockphoto-1317323736-612x612.jpg")
+hist,bins = np.histogram(image.fllaten(),bins=256,range=[0,256]=)
+
+   
+
+    
